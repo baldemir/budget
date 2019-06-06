34bac4e80499f67004484d2f46605c9b9a6b30a9
Beraat ALDEMİR (ULAKBİM) <beraat.aldemir@tubitak.gov.tr>
2019-06-06 15:49:34 +0300

    transaction store api method added

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -22,7 +22,39 @@ class TransactionController extends BaseController
         $products = Spending::all();
 
 
-        return $this->sendResponse($products->toArray(), 'Products retrieved successfully.');
+        return $this->sendResponse($products->toArray(), 'Transactions retrieved successfully.');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'space_id' => 'required',
+            'tag_id' => 'required'
+        ]);
+
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+
+        try{
+            $product = Spending::create($input);
+        }catch (\Exception $e){
+            return $this->sendError($e->getMessage());
+        }
+
+
+
+        return $this->sendResponse($product->toArray(), 'Product created successfully.');
     }
 
 }
