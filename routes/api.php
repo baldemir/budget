e4d1df5cfc6317451b113fc53114d884a37e5e2d
Beraat ALDEMİR (ULAKBİM) <beraat.aldemir@tubitak.gov.tr>
2019-06-06 15:50:31 +0300

    transaction store api method added

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,5 @@ use Illuminate\Http\Request;
 
 Route::group(['middleware' => 'auth:api'], function() {
     Route::get('getTransactions', 'API\TransactionController@index');
+    Route::get('setTransaction', 'API\TransactionController@store');
 });
