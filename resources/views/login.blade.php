75ec9c60309ad0719622b3579881a872d75bebf9
Beraat ALDEMİR (ULAKBİM) <beraat.aldemir@tubitak.gov.tr>
2019-06-17 16:59:36 +0300

    giris edit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
     <div class="wrapper wrapper--narrow my-3">
-        <h2 class="text-center mb-3">Log in</h2>
+        <h2 class="text-center mb-3">{{ __('general.login') }}</h2>
         @if (session('alert_type') && session('alert_message'))
             @include('partials.alerts.' . session('alert_type'), ['payload' => ['classes' => 'mb-2', 'message' => session('alert_message')]])
         @endif
