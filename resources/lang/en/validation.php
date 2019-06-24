eebe3aaa63f5edd28287827d70cfd1d2edeee091
Beraat ALDEMİR (ULAKBİM) <beraat.aldemir@tubitak.gov.tr>
2019-06-24 15:57:16 +0300

    translate added

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -3,6 +3,7 @@
 return [
     'required' => 'Can\'t be blank',
     'confirmed' => 'Doesn\'t match',
+    'unique' => 'Already taken',
     'max' => [
         'string' => 'Can\'t be longer than :max characters'
     ]
--- resources/lang/tr/validation.php
+++ resources/lang/tr/validation.php
@@ -3,6 +3,7 @@
 return [
     'required' => 'Boş bırakılamaz',
     'confirmed' => 'Eşleşmiyor',
+    'unique' => 'Daha önce alınmış',
     'max' => [
         'string' => ':max karakterden daha uzun olamaz'
     ]
