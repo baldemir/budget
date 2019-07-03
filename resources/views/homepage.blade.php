10dcd134a790f060d14b93214b227828bb95f743
Beraat ALDEMİR (ULAKBİM) <beraat.aldemir@tubitak.gov.tr>
2019-07-03 16:29:46 +0300

    favicon eklendi

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,6 +2,24 @@
 <html>
 <head>
     <title>Kolay Bütçe</title>
+    <link rel="apple-touch-icon" sizes="57x57" href="/storage/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/storage/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/storage/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/storage/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/storage/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/storage/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/storage/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/storage/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/storage/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/storage/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/storage/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/storage/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/storage/favicon-16x16.png">
+    <link rel="manifest" href="/storage/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/storage/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous" />
     <style>
         * {
