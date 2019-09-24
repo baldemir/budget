447fe5c9adda947112a37ee47bb3f3b79b688328
Beraat ALDEMİR (ULAKBİM) <beraat.aldemir@tubitak.gov.tr>
2019-09-24 18:09:01 +0300

    FB loginde olusturulan kategoriler gonderilen cihaz diline gore olusturulmaya baslandi

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -20,6 +20,7 @@ use Validator;
 
 class LoginController extends BaseController
 {
+
     /**
      * @bodyParam email string required Email of user.
      * @bodyParam password string required Password of user.
@@ -72,7 +73,7 @@ class LoginController extends BaseController
                 $space->save();
                 $createdUser->spaces()->attach($space->id, ['role' => 'admin']);
                 Mail::to($createdUser->email)->queue(new VerifyRegistration($createdUser));
-                $this->createDefaultCategories($createdUser);
+                $this->createDefaultCategories($createdUser, $request->get('deviceLanguage'));
             }else{
                 $existingUser->facebook_id = $user->getId();
                 $existingUser->save();
@@ -88,21 +89,60 @@ class LoginController extends BaseController
         }
     }
 
-    public function createDefaultCategories($user){
+    public function createDefaultCategories($user, $lang="tr"){
 
-        $userDefault = User::find(1);
-        $space = $userDefault->spaces()->first();
-        $userTags = $space->tags()->get();
+
+        $defaultCategoriesTr = array(
+            ["name" => "Market", "color" => Tag::randomColor(), "image" => "icon-category-grocery.png"],
+            ["name" => "Giyim / Aksesuar", "color" => Tag::randomColor(), "image" => "icon-category-shopping.png"],
+            ["name" => "Akaryakıt", "color" => Tag::randomColor(), "image" => "icon-category-auto.png"],
+            ["name" => "Yeme / İçme", "color" => Tag::randomColor(), "image" => "icon-category-food.png"],
+            ["name" => "Fatura", "color" => Tag::randomColor(), "image" => "icon-category-office.png"],
+            ["name" => "Kira", "color" => Tag::randomColor(), "image" => "icon-category-rent.png"],
+            ["name" => "Aidat", "color" => Tag::randomColor(), "image" => "icon-category-bank.png"],
+            ["name" => "Seyahat", "color" => Tag::randomColor(), "image" => "icon-category-travel.png"],
+            ["name" => "Eğitim", "color" => Tag::randomColor(), "image" => "icon-category-education.png"],
+            ["name" => "Ulaşım", "color" => Tag::randomColor(), "image" => "icon-category-transport.png"],
+            ["name" => "Eğlence / Hobi", "color" => Tag::randomColor(), "image" => "icon-category-entertainment.png"],
+            ["name" => "Sağlık", "color" => Tag::randomColor(), "image" => "icon-category-medical.png"],
+            ["name" => "Hediye", "color" => Tag::randomColor(), "image" => "icon-category-gift.png"],
+            ["name" => "Kişisel Bakım", "color" => Tag::randomColor(), "image" => "icon-category-pet.png"],
+            ["name" => "Vergi", "color" => Tag::randomColor(), "image" => "icon-category-tax.png"],
+            ["name" => "Diğer", "color" => Tag::randomColor(), "image" => "icon-category-misc.png"]);
+
+        $defaultCategoriesEn = array(
+            ["name" => "Groceries", "color" => Tag::randomColor(), "image" => "icon-category-grocery.png"],
+            ["name" => "Clothing", "color" => Tag::randomColor(), "image" => "icon-category-shopping.png"],
+            ["name" => "Fuel", "color" => Tag::randomColor(), "image" => "icon-category-auto.png"],
+            ["name" => "Food", "color" => Tag::randomColor(), "image" => "icon-category-food.png"],
+            ["name" => "Utilities", "color" => Tag::randomColor(), "image" => "icon-category-office.png"],
+            ["name" => "Housing", "color" => Tag::randomColor(), "image" => "icon-category-rent.png"],
+            ["name" => "Fee", "color" => Tag::randomColor(), "image" => "icon-category-bank.png"],
+            ["name" => "Travel", "color" => Tag::randomColor(), "image" => "icon-category-travel.png"],
+            ["name" => "Education", "color" => Tag::randomColor(), "image" => "icon-category-education.png"],
+            ["name" => "Transportation", "color" => Tag::randomColor(), "image" => "icon-category-transport.png"],
+            ["name" => "Entertainment", "color" => Tag::randomColor(), "image" => "icon-category-entertainment.png"],
+            ["name" => "Healthcare", "color" => Tag::randomColor(), "image" => "icon-category-medical.png"],
+            ["name" => "Gift", "color" => Tag::randomColor(), "image" => "icon-category-gift.png"],
+            ["name" => "Personal Care", "color" => Tag::randomColor(), "image" => "icon-category-pet.png"],
+            ["name" => "Tax", "color" => Tag::randomColor(), "image" => "icon-category-tax.png"],
+            ["name" => "Misc", "color" => Tag::randomColor(), "image" => "icon-category-misc.png"]);
+
+
         $spaceId = $user->spaces()->first()->id;
-        foreach ($userTags as $userTag){
+        $cats=$defaultCategoriesTr;
+        if($lang != "tr"){
+            $cats = $defaultCategoriesEn;
+        }
+        foreach ($cats as $userTag){
+
             Tag::create([
                 'space_id' => $spaceId,
-                'name' => $userTag->name,
-                'color' => $userTag->color,
-                'image' => $userTag->image
+                'name' => $userTag["name"],
+                'color' => $userTag["color"],
+                'image' => str_replace('/storage/category/', '', $userTag["image"])
             ]);
         }
-
     }
 
 
