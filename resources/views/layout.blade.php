ee7e8cc0e5e63175eb93d2e862653aae70b73c81
Beraat ALDEMİR (ULAKBİM) <beraat.aldemir@tubitak.gov.tr>
2019-09-27 14:25:01 +0300

    favicon updated

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,19 +3,19 @@
     <head>
         <title>{{ View::hasSection('title') ? View::getSection('title') . ' - ' . config('app.name') : config('app.name') }}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/storage/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="/storage/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="/storage/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="/storage/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="/storage/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="/storage/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="/storage/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="/storage/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="/storage/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="/storage/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/storage/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="/storage/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/storage/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="/storage/soft_files/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/storage/soft_files/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/storage/soft_files/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/storage/soft_files/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/storage/soft_files/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/storage/soft_files/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/storage/soft_files/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/storage/soft_files/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/storage/soft_files/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/storage/soft_files/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/storage/soft_files/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/storage/soft_files/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/storage/soft_files/favicon-16x16.png">
         <link rel="manifest" href="/storage/manifest.json">
         <meta name="msapplication-TileColor" content="#ffffff">
         <meta name="msapplication-TileImage" content="/storage/ms-icon-144x144.png">
